ae208f250867e591797de95fd1ed16f8f8b3a5ae
Charudutta <restrictedcharudutta@gmail.com>
2021-12-06 23:43:01 +0530

    minor fixes

---
+++ client/src/Componenets/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='main-nav'>
+            <>
+            </>
+        </div>
+    )
+}
+
+export default Navbar;
